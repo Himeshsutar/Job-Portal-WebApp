76d74b0bd0832f51a456ea973d3a8a30384148e2
Himeshsutar <himeshsutar22@gmail.com>
2025-06-23 19:18:13 +0530

    Fix: Add Railway domain to ALLOWED_HOSTS

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['job-portal-webapp-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
