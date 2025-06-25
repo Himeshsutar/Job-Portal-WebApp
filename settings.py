630a6c197379105794e36c36632ee8f21b140db4
Himeshsutar <himeshsutar22@gmail.com>
2025-06-25 12:08:36 +0530

    Fix static files for production

--- settings.py
+++ settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [  # ✅ Where your original static files (CSS/JS/images) live
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
